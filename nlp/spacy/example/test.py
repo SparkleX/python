f03b303acb2993811e9bb2e8423c11260761336a
Sun <sunxufei@hotmail.com>
2019-11-12 12:46:49 +0800

    123

--- nlp/spacy/example/test.py
+++ nlp/spacy/example/test.py
@@ -1,10 +1,26 @@
 import spacy 
 from spacy import displacy	
 
+transactios = ['sale order','journal entry','purchase order', 'invoice']
 
 
-def nlp(line):
-  print(line)
+def findTransaction(name):
+	for t in transactios:
+		if name.find(t)>=0:
+			return t
+
+def nlp(text):
+	nlp = spacy.load("en")
+	doc = nlp(text)
+	normalized = '';
+	for token in doc:	
+		#print(token.text, token.lemma_, token.pos_, token.is_stop)
+		normalized = normalized + token.lemma_ + ' '
+	find = findTransaction(normalized)
+	if find is not None:
+		print("OPEN:", find)
+	else:
+		print("try 'open ...' e.g. open sales order")
 
 # main
 print("Charbot, type q to quit")
